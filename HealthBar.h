a1d6cac0655e2faeb92d665efe18ddcec9291264
Luis Felipe Alvarez <luisfeal1998@hotmail.com>
2018-04-10 12:42:18 -0600

    Text recognition

--- HealthBar.h
+++ HealthBar.h
@@ -1,32 +1,40 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
 using namespace sf;
 
 class HealthBar {
+	private:
+		RectangleShape bar;
+		int maxHealth;
+		int hp;
+		Vector2f pos;
+		Vector2f size;
+
 	public:
-		HealthBar() {
-			pos = Vector2f(0, 0);
+		//Normal hbar
+		HealthBar(Vector2f _pos) {
+			pos = _pos;
 			maxHealth = 100;
-			bar = RectangleShape(Vector2f(200,20));
-			bar.setPosition(pos);
-			bar.setFillColor(Color::Green);
-		}
-		HealthBar(Vector2f pos) {
-			this->pos = pos;
-			maxHealth = 100;
-			bar = RectangleShape(Vector2f(200, 20));
+			hp = 100;
+			bar = RectangleShape(Vector2f(hp / maxHealth * 200, 20));
 			bar.setPosition(pos);
 			bar.setFillColor(Color::Green);
 
 		}
+		//Black hbar
+		HealthBar(Vector2f pos, Color color) {
+			this->pos = pos;
+			bar = RectangleShape(Vector2f(200, 20));
+			bar.setPosition(pos);
+			bar.setFillColor(color);
+
+		}
+		
 		void draw(RenderWindow *window) {
 			window->draw(bar);
 		}
 
-	private:
-		RectangleShape bar;
-		int maxHealth;
-		Vector2f pos;
-		Vector2f size;
 
 
 };
--- main.cpp
+++ main.cpp
@@ -1,30 +1,57 @@
 #include "Player.h"
 #include "HealthBar.h"
 
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(500, 500), "TypeRumble");
 	Player playerOne;
 	HealthBar hb1(Vector2f(0,0));
 	HealthBar hb2(Vector2f(window.getSize().x - 200,0));
-	
+	HealthBar hb3(Vector2f(0, 0), Color::Black);
+
+	Font font;
+	if (!font.loadFromFile("Raleway-Regular.ttf")) {
+		cout << "error" << "\n";
+	}
+	Text text;
+	text.setFont(font);
+	text.setFillColor(Color::Red);
+	//text.setString("hola");
+	string sentence;
 	while (window.isOpen())
 	{
-		sf::Event event;
+		Event event;
+		
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			switch (event.type) {
+			case Event::Closed:
 				window.close();
+				break;
+				//typing
+			case Event::TextEntered:
+				if (event.text.unicode < 128) {
+					sentence += static_cast<char>(event.text.unicode);
+				}
+				break;
+			}
 		}
-
-		window.clear();
-
+		text.setString(sentence);
+		
+		window.clear(Color::White);
 		//playerOne.draw(&window);
+		hb3.draw(&window);
+		
 		hb1.draw(&window);
 		hb2.draw(&window);
-
+	
+		//hb1.inflictDamage();
+		
+		window.draw(text);
 		window.display();
 	}
+	
 
 	return 0;
 }
